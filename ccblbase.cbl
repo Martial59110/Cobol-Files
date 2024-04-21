b8391839cdb2aa2cbba85bde61089fceb4838b08
Martial <floquet.martial@gmail.com>
2024-04-21 22:15:36 +0200

    Feat : Total

--- ccblbase.cbl
+++ ccblbase.cbl
@@ -44,23 +44,29 @@
             05 NUMBER1 PIC X(8).
             05 NUMBER2 PIC X(11).
             05 NUMBER3 PIC 9(8).
+           
             05 EURO PIC X(3).
         01 WS-TIRET PIC X(50).
+        01 TOTAL PIC 9(8).
+        01 TOTALCLEAN PIC X(9).
+        01 VIRGULE PIC X VALUE ",".
         01 WS-SPACE PIC X(50).
         01 WS-COUNT PIC 9(2).
-        01 WS-DISPLAY PIC X(60) VALUE  "Liste des libellés et des statu
-      -    "ts :".
+        01 WS-DISPLAY PIC X(60) VALUE  "Liste des clients :".
         01 WS-DISPLAY1 PIC X(30) VALUE  "Nombre d'enregistrements :".
         01 WS-INSPECT PIC X(8) VALUE "Actif".
         01 WS-INSPECT-COUNT-A PIC 9(2).
         01 WS-INSPECT-COUNT-R PIC 9(2).
         01 WS-INSPECT-COUNT-S PIC 9(2).
         01 WS-DISPLAY2 PIC X(30) VALUE "Nombre d'Actifs :".
-        01 WS-DISPLAY-LI PIC X(15) VALUE "Libellé :".
+        01 WS-DISPLAY-LI PIC X(20).
         01 WS-DISPLAY-LIFULL PIC X(26).
+        
+       01  WS-SC-LINE PIC 9.
+       01  WS-SC-CLR-A PIC 9.
+       01  WS-SC-CLR-B PIC 9.
        
-      
-      
+
        PROCEDURE DIVISION.
            MOVE ALL "-" TO WS-TIRET.
            MOVE ALL " " TO WS-SPACE.
@@ -106,7 +112,13 @@
            Move "Nombre de suspendus:" TO WS-DISPLAY2.
            WRITE F-DATA-RAPPORT FROM WS-DISPLAY2. 
            WRITE F-DATA-RAPPORT FROM WS-INSPECT-COUNT-S.
-           
+           MOVE TOTAL TO TOTALCLEAN(1:6).
+           MOVE TOTAL(7:2) TO TOTALCLEAN(8:2).
+           MOVE VIRGULE TO TOTALCLEAN(7:1).
+           Move "ARGENT TOTAL:" TO WS-DISPLAY2.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY2.
+           WRITE F-DATA-RAPPORT FROM TOTALCLEAN.
+
            CLOSE ASSU2.
            CLOSE ASSU-RAPPORT.
            STOP RUN.
@@ -118,7 +130,7 @@
            READ ASSU INTO F-DATA
            INSPECT F-DATA REPLACING ALL "*" BY " "
            ADD 1 TO WS-COUNT.
-
+           
       *    Déplacement des données du fichier dans mon tableau.
       *    Recherche des actifs etc... avec Inspect puis ajout au 
       *     compteur.
@@ -126,30 +138,54 @@
       *    de sortie.
 
            MOVE F-DATA TO ARRAY(WS-IDX).
+           ADD NUMBER3(WS-IDX) TO TOTAL.
            MOVE "Actif" TO WS-INSPECT.
            INSPECT STATUT(WS-IDX) TALLYING WS-INSPECT-COUNT-A
            FOR ALL WS-INSPECT.
            MOVE "Resilie" TO WS-INSPECT.
            INSPECT STATUT(WS-IDX) TALLYING WS-INSPECT-COUNT-R
-           FOR ALL WS-INSPECT.     
+           FOR ALL WS-INSPECT.
            MOVE "Suspendu" TO WS-INSPECT.
            INSPECT STATUT(WS-IDX) TALLYING WS-INSPECT-COUNT-S
-           FOR ALL WS-INSPECT.   
-           MOVE "Libellé :" TO WS-DISPLAY-LI.
-
-           STRING WS-DISPLAY-LI DELIMITED BY SIZE, SPACE,
-           ID-NAME(WS-IDX)      DELIMITED BY SIZE
-                                INTO WS-DISPLAY-LIFULL.
-
-           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LIFULL.
+           FOR ALL WS-INSPECT.
+          
+          
+           MOVE "ID:" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM ID-NUM(WS-IDX).
+           MOVE "IRP:" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM LIBELLE(WS-IDX).
+           MOVE "Société :" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM DESCRIPTION(WS-IDX).
+           MOVE "SIREN 1 :" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM NUMBER1(WS-IDX).
+           
+           MOVE "SIREN 2 :" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM NUMBER2(WS-IDX). 
+            MOVE "Argent :" TO WS-DISPLAY-LI.
+            WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM NUMBER3(WS-IDX).
+            MOVE "Libellé :" TO WS-DISPLAY-LI.
+            WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM ID-NAME(WS-IDX).
            MOVE "Statut :" TO WS-DISPLAY-LI.
-
-           STRING WS-DISPLAY-LI DELIMITED BY SIZE, SPACE,
-           STATUT(WS-IDX)       DELIMITED BY SIZE
-                                INTO WS-DISPLAY-LIFULL.
-
-           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LIFULL.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM STATUT(WS-IDX).
            WRITE F-DATA-RAPPORT FROM WS-TIRET.
+
+            
 
          LIRE-FICHIER2.
 
@@ -158,10 +194,10 @@
            READ ASSU2 INTO F-DATA2.
            INSPECT F-DATA2 REPLACING ALL "*" BY " ".
            ADD 1 TO WS-COUNT.
-
+           
+           
            MOVE F-DATA2 TO ARRAY(WS-IDX).
-           INSPECT ARRAY(WS-IDX) TALLYING WS-INSPECT-COUNT-S
-           FOR ALL WS-INSPECT.
+           ADD NUMBER3(WS-IDX) TO TOTAL.
            MOVE "Actif" TO WS-INSPECT.
            INSPECT STATUT(WS-IDX) TALLYING WS-INSPECT-COUNT-A
            FOR ALL WS-INSPECT.
@@ -171,20 +207,40 @@
            MOVE "Suspendu" TO WS-INSPECT.
            INSPECT STATUT(WS-IDX) TALLYING WS-INSPECT-COUNT-S
            FOR ALL WS-INSPECT.
-           MOVE "Libellé :" TO WS-DISPLAY-LI.
-
-           STRING WS-DISPLAY-LI DELIMITED BY SIZE, SPACE,
-           ID-NAME(WS-IDX)      DELIMITED BY SIZE
-                                INTO WS-DISPLAY-LIFULL.
-
-           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LIFULL.
+          
+           MOVE "ID:" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM ID-NUM(WS-IDX).
+           MOVE "IRP:" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM LIBELLE(WS-IDX).
+           MOVE "Société :" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM DESCRIPTION(WS-IDX).
+           MOVE "SIREN 1 :" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM NUMBER1(WS-IDX).
+           
+           MOVE "SIREN 2 :" TO WS-DISPLAY-LI.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM NUMBER2(WS-IDX). 
+            MOVE "Argent :" TO WS-DISPLAY-LI.
+            WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM NUMBER3(WS-IDX).
+            MOVE "Libellé :" TO WS-DISPLAY-LI.
+            WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM ID-NAME(WS-IDX).
            MOVE "Statut :" TO WS-DISPLAY-LI.
-
-           STRING WS-DISPLAY-LI DELIMITED BY SIZE, SPACE,
-           STATUT(WS-IDX)       DELIMITED BY SIZE
-                                INTO WS-DISPLAY-LIFULL.
-
-           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LIFULL.
+           WRITE  F-DATA-RAPPORT FROM SPACE.
+           WRITE F-DATA-RAPPORT FROM WS-DISPLAY-LI.
+           WRITE F-DATA-RAPPORT FROM STATUT(WS-IDX).
            WRITE F-DATA-RAPPORT FROM WS-TIRET.
 
             
